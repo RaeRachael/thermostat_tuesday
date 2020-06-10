369e203ec6e85b78eb6542b906d7474f489f2ea6
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-10 15:10:08 +0100

    static page functionality for up/down temp + powerSaveOn/Off

--- jquery.js
+++ jquery.js
@@ -2,4 +2,23 @@ $(document).ready( function() {
   const thermostat = new Thermostat();
   console.log(thermostat)
   $('#getTemperature').text(thermostat.getTemperature());
+
+  $('#upTemp').click( function() {
+    thermostat.upTemp()
+    $('#getTemperature').text(thermostat.getTemperature());
+  });
+
+  $('#downTemp').click( function() {
+    thermostat.downTemp()
+    $('#getTemperature').text(thermostat.getTemperature());
+  });
+
+  $('#powerSaveOn').click( function() {
+    thermostat.powerSaveOn()
+  });
+
+  $('#powerSaveOff').click( function() {
+    thermostat.powerSaveOff()
+  });
+
 });
