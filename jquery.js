ade19f14fdad966d9c12e6d79e5b11db3277e735
Rachael <rachaelewins@hotmail.com>
2020-06-10 15:37:44 +0100

    colours for energy usage

--- jquery.js
+++ jquery.js
@@ -2,16 +2,18 @@ $(document).ready( function() {
   const thermostat = new Thermostat();
   console.log(thermostat)
   $('#getTemperature').text(thermostat.getTemperature());
-  $('#energyUsage').css('background-color', 'orange');
+  $('#energyUsage').css('background-color', colour());
 
   $('#upTemp').click( function() {
     thermostat.upTemp()
     $('#getTemperature').text(thermostat.getTemperature());
+    $('#energyUsage').css('background-color', colour());
   });
 
   $('#downTemp').click( function() {
     thermostat.downTemp()
     $('#getTemperature').text(thermostat.getTemperature());
+    $('#energyUsage').css('background-color', colour());
   });
 
   $('#powerSaveOn').click( function() {
@@ -25,6 +27,19 @@ $(document).ready( function() {
   $('#resetTemp').click( function() {
     thermostat.resetTemp();
     $('#getTemperature').text(thermostat.getTemperature());
+    $('#energyUsage').css('background-color', colour());
   });
+
+  function colour() {
+    var usage
+    usage = thermostat.energyUsage()
+    if (usage === 'low-usage') {
+      return 'green';
+    } else if (usage === 'medium-usage') {
+      return 'orange';
+    } else {
+      return 'red';
+    }
+  }
 
 });
